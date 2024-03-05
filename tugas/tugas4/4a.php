28beeab393dfc5af568e382b9da659f93cb34082
emmirfahrezi <emmir.fahrezi1@gmail.com>
2024-03-05 17:46:59 +0700

    tugas array

---
+++ tugas/tugas4/4a.php
@@ -0,0 +1,4 @@
+<?php
+$huruf = ["epan", "cipta", "akan", "u", "asa", "esok"];
+
+echo "M" . $huruf[4] . " " . "d" . $huruf[0] . "mu di" . $huruf[1] .  "kan oleh apa yang kau kerj" . $huruf[2] . " " . "hari ini, " . "b" . $huruf[3] . "kan b" . $huruf[5];
