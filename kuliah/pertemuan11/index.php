1d88b197acd9335ff4e9caa15058c5ce593ef17c
emmirfahrezi <emmir.fahrezi1@gmail.com>
2024-05-14 11:18:14 +0700

    dikelas

--- kuliah/pertemuan11/index.php
+++ kuliah/pertemuan11/index.php
@@ -1,15 +1,6 @@
 <?php
-// koneksi ke database
-$conn = mysqli_connect('localhost', 'root', '', 'pw2024_233040054')  or die('koneksi ke DB gagal');
-// lakukan query untuk mengambil data mahasiswa
-$result = mysqli_query($conn, "SELECT * FROM mahasiswa") or die(mysqli_error($conn));
-// menyiapkan data mahasiswa (fetch)
-$rows = [];
-while ($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row;
-}
-
-$mahasiswa = $rows;
+include 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
 
 ?>
 
@@ -26,6 +17,8 @@ $mahasiswa = $rows;
 <body>
     <div class="container">
         <h1>Daftar mahasiswa</h1>
+
+        <a href="tambah.php" class="btnbtn-primary">Tambah data mahasiswa</a>
         <table class="table">
             <thead>
                 <tr>
@@ -38,9 +31,10 @@ $mahasiswa = $rows;
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($mahasiswa as $mhs) : ?>
+                <?php $i = 1;
+                foreach ($mahasiswa as $mhs) : ?>
                     <tr>
-                        <th scope="row"><?= $mhs['id']; ?></th>
+                        <th scope="row"><?= $i++; ?></th>
                         <td><?= $mhs['nama']; ?></td>
                         <td><?= $mhs['nim']; ?></td>
                         <td><?= $mhs['email']; ?></td>
@@ -56,5 +50,3 @@ $mahasiswa = $rows;
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
-
-</html>
---
+++ kuliah/pertemuan11/tambah.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>pemrograman web | tambah data mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Tambah Data mahasiswa</h1>
+
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="nama" class="form-label">nama</label>
+                <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </form>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="nim" class="form-label">nim</label>
+                <input type="text" class="form-control" id="nim" name="nim">
+            </div>
+        </form>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="email" class="form-label">email</label>
+                <input type="text" class="form-control" id="email" name="email">
+            </div>
+        </form>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="jurusan" class="form-label">jurusan</label>
+                <input type="text" class="form-control" id="jurusan" name="jurusan">
+            </div>
+        </form>
+        <button type="suubmit" name="tambah" class="btn btn-primary">tambah</button>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
